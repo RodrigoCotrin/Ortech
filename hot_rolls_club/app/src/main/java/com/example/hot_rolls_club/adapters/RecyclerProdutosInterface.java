5d08a096440404ddc0194ba922091912a2aa7ad6
Rodrigo Cotrin <89107730+RodrigoCotrin@users.noreply.github.com>
2023-12-18 14:19:22 -0300

    Delete hot_rolls_club directory

--- hot_rolls_club/app/src/main/java/com/example/hot_rolls_club/adapters/RecyclerProdutosInterface.java
+++
@@ -1,5 +0,0 @@
-package com.example.hot_rolls_club.adapters;
-
-public interface RecyclerProdutosInterface {
-    void onItemClick(int position);
-}
--- hot_rolls_club/app/src/main/java/com/example/hot_rolls_club/classes/CategoriaSelecioada.java
+++
@@ -1,8 +0,0 @@
-package com.example.hot_rolls_club.classes;
-
-public class CategoriaSelecioada {
-    static int categoria = 0;
-
-    public static int getCategoria() {return categoria;}
-    public static void setCategoria(int categoria) {CategoriaSelecioada.categoria = categoria;}
-}
